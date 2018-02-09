61fe743381619ae6112f1f03f4189692210d297c
SeleznevDenis <d.selezneww@yandex.ru>
2018-02-09 23:43:08 +0500

    1.3 JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,17 @@
 package ru.job4j;
-
- public class Calculate {
- 
-    public static void main(String[] args) {
+/**
+*Calculate.
+*
+*@author Denis Seleznev (d.selezneww@mail.ru)
+*/
+ public class Calculate 
+ {
+    /**
+	*Main.
+	*@param args - args.
+	*/
+    public static void main(String[] args) 
+	{
         System.out.println("Hello World");
     }
  }
