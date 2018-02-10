6788e35a70b6df45e998720ea403240b69a1c0fa
SeleznevDenis <d.selezneww@yandex.ru>
2018-02-10 14:30:59 +0500

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,10 +3,12 @@ package ru.job4j;
 *Calculate.
 *
 *@author Denis Seleznev (d.selezneww@mail.ru)
+*@version $Id$
+*@since 0.1
 */
  public class Calculate 
  {
-    /**
+	/**
 	*Main.
 	*@param args - args.
 	*/
@@ -14,4 +16,13 @@ package ru.job4j;
 	{
         System.out.println("Hello World");
     }
+	/**
+	*Method echo.
+	*@param name Your name.
+	*@return Echo plus your name.
+	*/
+	public String echo(String name)
+	{
+		return "Echo, echo, echo : "+name;
+	}	
  }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Test.
+*
+*@author Denis Seleznev (d.selezneww@mail.ru)
+*@version $Id$
+*@since 0.1
+*/
+public class CalculateTest
+{
+	/**
+	*Test echo.
+	*/ @Test
+	public void whenTakeNameThenThreeEchoPlusName()
+	{
+		String input = "Denis Seleznev";
+		String expect = "Echo, echo, echo : Denis Seleznev";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}	
+}	
