25c6b81f5674c7ae1d94e605cb94e80143ae85d1
SeleznevDenis <d.selezneww@yandex.ru>
2018-03-19 14:34:37 +0500

    2. Сортировка User с использованием Comparator

--- chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sorting;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -18,5 +19,41 @@ public class SortUser {
      */
     public Set<User> sort (List<User> list) {
         return new TreeSet<>(list);
+    }
+
+    /**
+     * Упорядочивает пользователей User по длинне имени в порядке увеличения длины.
+     * @param list заданный для сортировки.
+     * @return отсортированный List.
+     */
+    public List<User> sortNameLength (List<User> list) {
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getName().length() - o2.getName().length();
+                    }
+                }
+        );
+        return list;
+    }
+
+    /**
+     * Упорядочивает пользователей User по имени в лексикографическом
+     * порядке и по возрасту в порядке возрастания, если имена одинаковые.
+     * @param list заданный для сортировки.
+     * @return отсортированный List.
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getName().equals(o2.getName()) ?
+                                o1.getAge() - o2.getAge() : o1.getName().compareTo(o2.getName());
+                    }
+                }
+        );
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
@@ -25,4 +25,31 @@ public class SortUserTest {
         Set<User> expect = new LinkedHashSet<>(asList(first, second, third));
         assertThat(result, is(expect));
     }
+    /**
+     * Test sortNameLength.
+     */
+    @Test
+    public void ifSortUsersListByNameLengthThenReturnSortedList() {
+        User first = new User("Egor", 10);
+        User second = new User("Roman", 25);
+        User third = new User("Vasiliy", 20);
+        SortUser testSort = new SortUser();
+        List<User> result = testSort.sortNameLength(new ArrayList<>(asList(first, third, second)));
+        List<User> expect = new ArrayList<>(asList(first, second, third));
+        assertThat(result, is(expect));
+    }
+    /**
+     * Test sortByAllFields.
+     */
+    @Test
+    public void ifSortUserListByAllFieldsThenReturnSortedList() {
+        User first = new User("Egor", 10);
+        User second = new User("Roman", 25);
+        User third = new User("Egor", 20);
+        SortUser testSort = new SortUser();
+        List<User> result = testSort.sortByAllFields(new ArrayList<>(asList(first, second, third)));
+        List<User> expect = new ArrayList<>(asList(first, third, second));
+        assertThat(result, is(expect));
+
+    }
 }
