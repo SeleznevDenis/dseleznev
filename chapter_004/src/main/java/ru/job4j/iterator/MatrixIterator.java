7218bb4cd710e5cb6e17c219290a08ba7288b208
SeleznevDenis <d.selezneww@yandex.ru>
2018-03-28 23:02:43 +0500

    5.1.1. Итератор для двухмерного массива int[][] [#9539]

---
+++ chapter_004/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,53 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Итератор для двумерного массива.
+ * @author Denis Seleznev
+ * @version $Id$
+ * @since 28.03.2018
+ */
+public class MatrixIterator implements Iterator {
+
+    private final int[][] array;
+    private int row;
+    private int column;
+
+    /**
+     * Конструктор, инициализирует поле array.
+     * @param array двумерный массив.
+     */
+    public MatrixIterator(int[][] array) {
+        this.array = array;
+    }
+
+    /**
+     * Проверяет наличие следующего элемента.
+     * @return true - следующий элемент есть,
+     *         false - массив закончился.
+     */
+    @Override
+    public boolean hasNext() {
+        return (row < array.length);
+    }
+
+    /**
+     * Возвращает текущий элемент и сдвигает указатели.
+     * @return текущий элемент массива.
+     * @throws NoSuchElementException - в случае пустого массива.
+     */
+    @Override
+    public Object next() {
+        if (this.row >= array.length) {
+            throw new NoSuchElementException();
+        }
+        int result = array[row][column++];
+        if (column >= array[row].length) {
+            column = 0;
+            row++;
+        }
+        return result;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/iterator/MatrixTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test MatrixIterator для квадратной матрицы.
+ * @author Denis Seleznev.
+ * @version $Id$
+ * @since 28.03.2018
+ */
+public class MatrixTest {
+
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new MatrixIterator(new int[][]{{1, 2, 3}, {4, 5, 6}});
+    }
+
+    @Test
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shoulThrowNoSuchElementException() {
+        it = new MatrixIterator(new int[][]{});
+        it.next();
+    }
+}
