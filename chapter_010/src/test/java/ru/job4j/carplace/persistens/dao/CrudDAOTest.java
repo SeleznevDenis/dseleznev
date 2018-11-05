8434edc24e869640a6004a19c5dc80b41ff76390
Denis Seleznev <d.selezneww@mail.ru>
2018-11-05 13:45:48 +0500

    test6

--- chapter_010/src/test/java/ru/job4j/carplace/persistens/dao/CrudDAOTest.java
+++ chapter_010/src/test/java/ru/job4j/carplace/persistens/dao/CrudDAOTest.java
@@ -1,90 +1,80 @@
 package ru.job4j.carplace.persistens.dao;
 
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseConnection;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.FileSystemResourceAccessor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.jdbc.Work;
-import org.junit.BeforeClass;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.carplace.persistens.interfaces.IEntity;
+import ru.job4j.annotationstest.models.Machine;
+import ru.job4j.carplace.persistens.models.Transmission;
 import ru.job4j.utils.SingletonSF;
 import ru.job4j.utils.TransactionWrapper;
-
-import java.sql.Connection;
-import java.sql.SQLException;
+import ru.job4j.annotationstest.annotationmodels.AEngine;
+import ru.job4j.annotationstest.annotationmodels.AGearBox;
+import ru.job4j.annotationstest.annotationmodels.AMachine;
+import ru.job4j.annotationstest.annotationmodels.ATransmission;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public abstract class CrudDAOTest<T extends IEntity> {
-
-    protected abstract CrudDAO<T> getTestDao();
-    protected abstract T getTestEntity();
-    protected abstract T getSecondTestEntity();
+public class CRUDDAOTest {
 
     private static final Logger LOGGER = LogManager.getLogger("servlets");
+    private static final SessionFactory SF = SingletonSF.getSessionFactory();
+    private static final TransactionWrapper WRAPPER = new TransactionWrapper(LOGGER, SF);
+    private CrudDAO<AMachine> testDao;
+    private ATransmission testTransmission;
+    private AEngine testEngine;
+    private AGearBox testGearBox;
+    private AMachine testMachine;
 
-    /**
-     * Выполняет команды с тестовым контекстом к базе данных , с помощью liquibase.
-     */
-    @BeforeClass
-    public static void insertToDB() {
-        new TransactionWrapper(LOGGER, SingletonSF.getSessionFactory()).wrapAndExecute(
-                session -> {
-                    session.doWork(new Work() {
-                        @Override
-                        public void execute(Connection con) throws SQLException {
-                            try {
-                                JdbcConnection connection = new JdbcConnection(con);
-                                Database base = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(connection);
-                                Liquibase liquibase = new Liquibase(
-                                        getClass().getClassLoader().getResource("db.changelog.xml").getFile(),
-                                        new FileSystemResourceAccessor(),
-                                        base
-                                );
+    @Before
+    public void setUp() {
+        this.testDao = new CrudDAO<AMachine>(SingletonSF.getSessionFactory()) { };
+        this.testTransmission = new ATransmission("testTransmission");
+        this.testEngine = new AEngine("testEngine");
+        this.testGearBox = new AGearBox("testGearBox");
+        this.testMachine = new AMachine("test", this.testTransmission, this.testEngine, this.testGearBox);
+    }
 
-                                liquibase.update("test");
-                                System.out.println("update");
-                            } catch (LiquibaseException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                    return null;
-                }
-        );
-
+    @After
+    public void cleanDB() {
+        WRAPPER.wrapAndExecute(session -> {
+            session.createQuery("delete from AMachine where name= 'test'").executeUpdate();
+            System.out.println("delete");
+            session.createQuery("delete from AEngine where description = 'testEngine'").executeUpdate();
+            session.createQuery("delete from AGearBox where description = 'testGearBox'").executeUpdate();
+            session.createQuery("delete from ATransmission where description = 'testTransmission'").executeUpdate();
+            return null;
+        });
     }
 
     @Test
     public void add() {
-        int entityId = this.getTestDao().add(this.getTestEntity());
-        T resultEntity = this.getTestDao().findById(entityId);
-        resultEntity.setId(0);
-        assertThat(resultEntity, is(this.getTestEntity()));
+        int carId = this.testDao.add(this.testMachine);
+        assertThat(this.testDao.findById(carId).getName(), is("test"));
     }
 
     @Test
     public void delete() {
-        int entityId = this.getTestDao().add(this.getTestEntity());
-        T entity = this.getTestEntity();
-        entity.setId(entityId);
-        this.getTestDao().delete(entity);
-        assertNull(this.getTestDao().findById(entityId));
+        int carId = this.testDao.add(this.testMachine);
+        this.testDao.delete(new AMachine(carId));
+        assertNull(this.testDao.findById(carId));
     }
 
     @Test
     public void update() {
-        int entityId = this.getTestDao().add(this.getTestEntity());
-        T upEntity = this.getSecondTestEntity();
-        upEntity.setId(entityId);
-        this.getTestDao().update(upEntity);
-        assertThat(this.getTestDao().findById(entityId), is(upEntity));
+        this.testMachine.setName("test2");
+        int carId = this.testDao.add(this.testMachine);
+        this.testMachine.setName("test");
+        this.testMachine.setId(carId);
+        this.testDao.update(this.testMachine);
+        assertThat(this.testDao.findById(carId).getName(), is("test"));
+    }
+
+    @Test
+    public void scratch() {
+
     }
 }
